9141f5d6fe217c8ab639db22ae89667bddeb87be
Amyth <aroras.official@gmail.com>
2016-08-27 20:59:13 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-liked',
+    version='1.0.0',
+    author='Amyth Arora',
+    author_email='mail@amythsingh.com',
+    packages=find_packages(),
+    url='https://github.com/amyth/django-liked',
+    license='MIT License',
+    description='A django likes app',
+    long_description='Django Liked is a generic like dislike application that '\
+        'built on django 1.10 using the contenttypes framework to create likes'\
+        'for any models in your django project'
+    zip_safe=False,
+)
